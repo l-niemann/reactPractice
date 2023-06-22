1e92988536c1580c42cbd5fd123e4554fbff5065
l-niemann <luke.niemann@discounttire.com>
2023-06-22 08:38:32 -0700

    Last commit for now

--- src/subApps/TodoList/TodoSection.js
+++ src/subApps/TodoList/TodoSection.js
@@ -5,14 +5,18 @@ import { AddTodo } from "./AddTodo";
 export function TodoSection({ name, sectionArr, setSectionArr }) {
   const [itemArr, setItemArr] = useState([]);
 
-  function handleDelete(){
+  function handleDelete() {
     let newArr = sectionArr.filter((element) => element.name !== name);
     setSectionArr(newArr);
-}
+  }
 
   return (
     <>
-      {itemArr.length === 0 ? <button className="todo-section-button" onClick={handleDelete}>X</button> : null}
+      {itemArr.length === 0 ? (
+        <button className="todo-section-button" onClick={handleDelete}>
+          X
+        </button>
+      ) : null}
       <h2 className="todo-section-p">{name}:</h2>
       {itemArr.map((item) => {
         return (
