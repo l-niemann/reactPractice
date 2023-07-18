63cf98b8827382fee0d9bb539889fa648f796943
l-niemann <luke.niemann@discounttire.com>
2023-07-18 15:47:35 -0700

    added todos

--- src/subApps/notes/Notes.js
+++ src/subApps/notes/Notes.js
@@ -4,17 +4,20 @@ import { InputToggle } from "./InputToggle";
 import { useState } from "react";
 
 export function Notes() {
-  const [notesArr,setNotesArr] = useState([]);
-//TODO: Make this look a little nicer. Add delete button
+  const [notesArr, setNotesArr] = useState([]);
+  //TODO: Make this look a little nicer. Add delete button
   return (
     <div>
       <HomeLink />
       <h1>Notes</h1>
       <InputToggle notesArr={notesArr} setNotesArr={setNotesArr} />
-      {console.log(notesArr)}
+      {
+        //TODO: Make each note have a unique key and make input...
+        //titles be different
+      }
       {notesArr.map((note) => {
         console.log(notesArr);
-        return (<Note title={note.title} desc={note.desc} />)
+        return <Note title={note.title} desc={note.desc} />;
       })}
     </div>
   );
